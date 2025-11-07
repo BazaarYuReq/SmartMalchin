4565bea82380d5656df52ed2d54cb9b29ab9855c
PADA-1-3-03 <pada-1-3-03@Erxess-iMac.local>
2025-11-07 20:36:17 +0800

    Inital commit

---
+++ app/components/Footer.js
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="bg-gray-900 text-gray-300 text-center p-4">
+      <p>© 2025 My Website. All rights reserved.</p>
+    </footer>
+  );
+}
--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Footer from "./components/Footer";
+import Account from "./components/Account";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -22,7 +24,8 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <main> {children}</main>
+        <Footer />
       </body>
     </html>
   );
